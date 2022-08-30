f251860bed28413c1f86af994aafa87a104dbfb3
Andrey <lutecequantum@gmail.com>
2022-08-30 20:00:42 +0300

    5.3 edited README.md brain_games/scripts/brain_even.py

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 [![Actions Status](https://github.com/MisterFlicker/python-project-49/workflows/hexlet-check/badge.svg)](https://github.com/MisterFlicker/python-project-49/actions)
 
 <a href="https://codeclimate.com/github/MisterFlicker/python-project-49/maintainability"><img src="https://api.codeclimate.com/v1/badges/50b7cdc90c8959ebe9ee/maintainability" /></a>
+
+https://asciinema.org/a/vwEcf4NtdV4k2nWJV6cfnB42x
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -6,38 +6,32 @@ from random import randint
 
 def welcome_user():
     name = prompt.string('May I have your name? ')
-    print(f'Hello, {name}!')
-    print('Answer "yes" if the number is even, otherwise answer "no".')
+    print(f'Hello, {name}!\nAnswer \"yes\" if the number is even, otherwise answer \"no\".')
     counter = 0
     while counter < 3:
-        random_number = randint(1, 100)
-        print(f'Question: {random_number}')
-        users_answer = prompt.string('Your answer: ')
-        if users_answer == 'yes' and random_number % 2 == 0:
+        number = randint(1, 100)
+        print(f'Question: {number}')
+        answer = prompt.string('Your answer: ')
+        if (answer == 'yes' and number % 2 == 0) or (answer == 'no' and number % 2 != 0):
             counter = counter + 1
             print('Correct!')
-        if users_answer == 'yes' and random_number % 2 != 0:
+        if answer == 'yes' and number % 2 != 0:
             counter = 0
             print("'yes' is wrong answer ;(. Correct answer was 'no'.")
             print(f'Let\'s try again, {name}!')
-        if users_answer == 'no' and random_number % 2 == 0:
+        if answer == 'no' and number % 2 == 0:
             counter = 0
             print("'no' is wrong answer ;(. Correct answer was 'yes'.")
             print(f'Let\'s try again, {name}!')
-        if users_answer == 'no' and random_number % 2 !=0:
-            counter = counter + 1
-            print('Correct!')
-        if users_answer != 'no' and users_answer != 'yes' and random_number % 2 == 0:
+        if answer != 'no' and answer != 'yes' and number % 2 == 0:
             counter = 0
-            print(f'{users_answer} is wrong answer ;(. Correct answer was \'yes\'.')
+            print(f'{answer} is wrong answer ;(. Correct answer was \'yes\'.')
             print(f'Let\'s try again, {name}!')
-        if users_answer != 'no' and users_answer != 'yes' and random_number % 2 != 0:
+        if answer != 'no' and answer != 'yes' and number % 2 != 0:
             counter = 0
-            print(f'{users_answer} is wrong answer ;(. Correct answer was \'no\'.')
+            print(f'{answer} is wrong answer ;(. Correct answer was \'no\'.')
             print(f'Let\'s try again, {name}!')
     print(f'Congratulations, {name}!')
-
-
 
 
 def main():
